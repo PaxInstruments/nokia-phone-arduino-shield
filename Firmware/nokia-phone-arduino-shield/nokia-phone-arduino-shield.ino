cd01c0d5c7d11d8374073875cea121f8fe1f6d23
Charles Edward Pax <charles.pax@gmail.com>
2015-05-20 01:52:21 +0800

    Added nearly blank Arduino sketch.

---
+++ Firmware/nokia-phone-arduino-shield/nokia-phone-arduino-shield.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void prepare() {
+ for (int i = 0; i < 128; i++) {
+  Serial.write(0x55):
+ }
+}
